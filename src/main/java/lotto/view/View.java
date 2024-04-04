24f512e60e0c52870d8d1c48c7a9bd240fc0959f
KimJS0328 <wjddkel328@gmail.com>
2024-04-04 11:19:05 +0900

    style: 불필요한 import 삭제

--- src/main/java/lotto/view/View.java
+++ src/main/java/lotto/view/View.java
@@ -4,7 +4,6 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.Scanner;
 import java.util.stream.Collectors;
-import java.util.stream.IntStream;
 import java.util.stream.Stream;
 
 import lotto.domain.Lotto;
